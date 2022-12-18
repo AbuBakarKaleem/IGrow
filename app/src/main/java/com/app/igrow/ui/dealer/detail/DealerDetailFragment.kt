d2666795883384fe28dce1a7cffbf853e685aab4
usama.yasin@is.com <usama.yasin@is.com>
2022-12-18 16:29:03 +0400

    bug fix youtube title search dialer intent

--- app/src/main/java/com/app/igrow/ui/dealer/detail/DealerDetailFragment.kt
+++ app/src/main/java/com/app/igrow/ui/dealer/detail/DealerDetailFragment.kt
@@ -1,14 +1,16 @@
 package com.app.igrow.ui.dealer.detail
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.app.igrow.base.BaseFragment
 import com.app.igrow.data.model.sheets.Dealers
 import com.app.igrow.databinding.FragmentDealerDetailBinding
 import com.app.igrow.ui.dealer.alldealers.DealersListFragment.Companion.ARG_DEALER_DETAIL_ITEM_KEY
-import com.app.igrow.ui.diagnose.DiagnoseFragment
 import com.app.igrow.utils.Utils.isLocaleFrench
 
 class DealerDetailFragment : BaseFragment<FragmentDealerDetailBinding>() {
@@ -20,6 +22,16 @@ class DealerDetailFragment : BaseFragment<FragmentDealerDetailBinding>() {
         arguments?.let {
             val itemArgs = it.get(ARG_DEALER_DETAIL_ITEM_KEY) as Dealers
             setPopulateViews(itemArgs)
+        }
+
+        binding.tvTelephone.setOnClickListener {
+            if ((it as TextView).text.toString().isEmpty().not())
+                openDialer((it).text.toString().trim())
+        }
+
+        binding.tvMobile.setOnClickListener {
+            if ((it as TextView).text.toString().isEmpty().not())
+                openDialer((it).text.toString().trim())
         }
     }
 
@@ -38,6 +50,12 @@ class DealerDetailFragment : BaseFragment<FragmentDealerDetailBinding>() {
         binding.tvAddress.text = value
         value = if(isLocaleFrench()) dealer.distributors_fr  else dealer.distributors
         binding.tvDistributors.text = value
+    }
+
+    private fun openDialer(number:String){
+        val intent = Intent(Intent.ACTION_DIAL)
+        intent.data = Uri.parse("tel:$number")
+        startActivity(intent)
     }
 
 }
